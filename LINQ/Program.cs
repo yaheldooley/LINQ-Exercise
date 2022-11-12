e3d6720e2ef8cc39f602ec900b8acc8b688429ff
yahel dooley <ydooley@gmail.com>
2022-11-11 21:31:04 -0800

    Made a list of video game titles and sort them in ascending order

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -4,7 +4,16 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var videoGames = new List<string>() {
+            "Tetris",
+            "Minecraft",
+            "Pac-Man",
+            "Dig Dug",
+            "Civilization",
+            };
+
+            var sortedGames = videoGames.OrderBy(name => name);
+            foreach (string game in sortedGames) Console.WriteLine($"{game}");
         }
     }
 }
